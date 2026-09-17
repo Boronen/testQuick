8a8f46e25caff862c64a5f1b087652849d90ab86
kovacs.kevin <kovacs.kevin@DESKTOP-6R31BP8.akk.lan>
2026-09-17 15:49:05 +0200

    mégegy commit

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -2,7 +2,11 @@ package javaapplication3;
 
 public class JavaApplication3 {
 
-        
+    public static void main(String[] args) {
+        System.out.println("Gubek Vera ♥");
+        System.out.println("Kovács Kevin");
+        System.out.println("Kiscica");
+    }        
 
     public static void main(String[] args) {
         System.out.println("Gubek Vera ♥");
