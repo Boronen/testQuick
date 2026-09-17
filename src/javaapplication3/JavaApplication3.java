67557778114be4b16a7a39caebf826c0938eb6e1
kovacs.kevin <kovacs.kevin@DESKTOP-6R31BP8.akk.lan>
2026-09-17 15:48:13 +0200

    Merge branch 'main' of https://github.com/Boronen/testQuick
    # Conflicts:
    #	src/javaapplication3/JavaApplication3.java

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -7,6 +7,7 @@ public class JavaApplication3 {
     public static void main(String[] args) {
         System.out.println("Gubek Vera ♥");
         System.out.println("Kovács Kevin");
+        System.out.println("Kiscica");
     }
     
 }
