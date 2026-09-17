e24c0b3be6287c2d16da5019222651f76cffe9cd
kovacs.kevin <kovacs.kevin@DESKTOP-6R31BP8.akk.lan>
2026-09-17 15:40:10 +0200

    nevem

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -4,17 +4,9 @@
  */
 package javaapplication3;
 
-/**
- *
- * @author kovacs.kevin
- */
 public class JavaApplication3 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Kovács Kevin");
     }
     
 }
